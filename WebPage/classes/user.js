20b8b5cab756679f2f4de45250c5ce896140da0b
charder <chardergaming@gmail.com>
2016-10-19 16:04:00 -0400

    Very deep and complex user.js file. Top Secret.
    I'm not joking guys.

---
+++ WebPage/classes/user.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(email, username, password) {
+        this.email = email;
+        this.username = username;
+	    this.password = password;
+    }
+  
+    get username() {
+        return this.username;
+    }
+  
+    get email() {
+        return this.email;
+    }
+	
+	get password() {
+		return this.password;
+	}
+  
+    set username(newUsername) {
+	    this.username = newUsername;
+    }
+	
+	set password(newPassword) {
+		this.password = newPassword;
+	}
+}
