8c337aea3729403f7dfd6d963ea7743b6a329949
goubew <goubew@users.noreply.github.com>
2016-11-02 14:21:56 -0400

    Changed API so all data is returned

--- public/WebPage/classes/user.js
+++ public/WebPage/classes/user.js
@@ -2,26 +2,26 @@ class User {
     constructor(email, username, password) {
         this.email = email;
         this.username = username;
-	    this.password = password;
+	      this.password = password;
     }
-  
+
     get username() {
         return this.username;
     }
-  
+
     get email() {
         return this.email;
     }
-	
-	get password() {
-		return this.password;
-	}
-  
+
+	  get password() {
+		    return this.password;
+	  }
+
     set username(newUsername) {
-	    this.username = newUsername;
+	      this.username = newUsername;
     }
-	
-	set password(newPassword) {
-		this.password = newPassword;
-	}
+
+	  set password(newPassword) {
+		    this.password = newPassword;
+	  }
 }
